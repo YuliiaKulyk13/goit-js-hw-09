fcf4913b52c8c1e7d8d574a88efeee484461cfe6
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-02-02 15:12:52 -0500

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('.data-start');
+const btnStop = document.querySelector('.data-stop');
+
+// let timerId = null;
+
+// const changeColor = event => {
+//   const colorFunction = getRandomHexColor();
+//   timerId = setInterval(() => {
+//     document.body.backgroundColor = colorFunction;
+//   }, 1000);
+// };
+
+// btnStop.addEventListener('click', () => {
+//   clearInterval(timerId);
+// });
+
+// btnStart.addEventListener('click', changeColor);
